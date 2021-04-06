d62cc8e3a93ea517b4d8d3fc521ff0af7fdc80c6
RoyNg88 <s3741190@rmit.edu.vn>
2021-04-07 01:54:09 +0700

    Update

---
+++ src/Asm1/Main.java
@@ -0,0 +1,12 @@
+package Asm1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       mainmenu();
+    }
+    public static void mainmenu(){
+
+    }
+}
